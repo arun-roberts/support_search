2c8bdfc13c8d9cd567fba7c91916ae69a25c76ea
Arun Roberts <arunplaysbass@gmail.com>
2022-11-22 17:31:37 +1100

    Initial commit. Success with HWLR

---
+++ venue_template.py
@@ -0,0 +1,44 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+# Making a GET request
+r = requests.get('https://www.moshtix.com.au/mosh2/rss/GenerateFeed.aspx?feedCode=636851343092690293')
+
+ 
+# Parsing the HTML
+soup = BeautifulSoup(r.content, 'xml')
+ 
+s = soup.find_all('item')
+ 
+gigs = []
+
+for each in s:
+    obj = { 
+        "title": each.title.string,
+        "description": each.description.string,
+        "artists": [],
+        "when": each.find('moshtix:eventstartdatetime').string,
+        "link": each.link.string,
+        "sold_out": each.find('moshtix:soldout').string,
+        "genre": each.genre.string
+    }
+    for artist in each.find('moshtix:artists'):
+        str = artist.string
+        if (str != "howler" and str != "hwlr" and str != "\n"):
+            obj["artists"].append(str)
+    gigs.append(obj)
+
+print(gigs)
+
+with open("hwlr_test.js", "w") as j:
+    json.dump(gigs, j)  
+
+# print(s)
+# t = open("test.txt", "a")
+# for line in lines:
+#     # print(line.text)
+#     t.write(str(line))
+#     t.write(', ')
+# t.close()
+ 
---
+++ venues/hwlr.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+# Making a GET request
+r = requests.get('https://www.moshtix.com.au/mosh2/rss/GenerateFeed.aspx?feedCode=636851343092690293')
+ 
+# Parsing the XML
+soup = BeautifulSoup(r.content, 'xml')
+ 
+s = soup.find_all('item')
+ 
+gigs = []
+
+for each in s:
+    obj = { 
+        "title": each.title.string,
+        "description": each.description.string,
+        "artists": [],
+        "when": each.find('moshtix:eventstartdatetime').string,
+        "link": each.link.string,
+        "sold_out": each.find('moshtix:soldout').string,
+        "genre": each.genre.string
+    }
+    for artist in each.find('moshtix:artists'):
+        str = artist.string
+        if (str != "howler" and str != "hwlr" and str != "\n"):
+            obj["artists"].append(str)
+    gigs.append(obj)
+
+with open("hwlr_test.js", "w") as j:
+    json.dump(gigs, j)  
+
