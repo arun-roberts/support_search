51b5a0e9906a645b6c5fdd5cd207deeaefc6da45
Arun Roberts <arunplaysbass@gmail.com>
2022-12-01 06:55:55 +1100

    Building out main GUI table.

--- main_gui.py
+++ main_gui.py
@@ -1,7 +1,10 @@
-import os
+import json
 import subprocess
 from tkinter import *
 from tkinter import ttk
+
+with open("prev_scrape_results.json", "r") as prev_results:
+    prev = json.load(prev_results)["data"]
 
 # def run():
 #     os.system('python3 "compare_scrape_results.py"')
@@ -9,9 +12,17 @@ def run():
     subprocess.run(["python3", "compare_scrape_results.py"])
 
 root = Tk()
-frm = ttk.Frame(root, padding=15)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Check for new gigs", command=run).grid(column=3, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=2)
+title_frame = ttk.Frame(root, padding=15)
+title_frame.justify(CENTER)
+ttk.Label(title_frame, text="Scrape it.")
+
+info_body = ttk.Frame(root, padding=15)
+info_body.grid()
+for i, gig in enumerate(prev):
+    ttk.Label(info_body, text=gig.title).grid(column=1, row=i)
+    ttk.Button(info_body, text="Go fish", command=fish).grid(column=3, row=i)
+    ttk.Button(info_body, text="Applied", command=checkItOff).grid(column=4, row=i)
+    ttk.Button(info_body, text="No", command=getOutOfHere).grid(column=5, row=i)
+
+ttk.Button(root, text="Exit", command=root.destroy)
 root.mainloop()
