b48025a32054f24247ddb138cacea2683e64598c
Arun Roberts <arunplaysbass@gmail.com>
2022-11-30 16:12:19 +1100

    GUI basic starter added.

--- venues/memo.py
+++ venues/memo.py
@@ -20,19 +20,19 @@ def gig_scraper(gigs_array):
             "artists": [each.select('h2.text-center a')[0].string, each.select('h4.gig-support-band-name')[0].string],
             "venue": "Memo Music Hall, St Kilda",
             "when": "",
-            "link": 'https://retreathotelbrunswick.squarespace.com' + each.find(class_='summary-title-link')['href'],
+            "link": each.select('h2.text-center a')['href'],
             "sold_out": "",
             "genre": ""
         }
-        gig_date = each.find(class_='summary-thumbnail-event-date-day').string
-        if (int(gig_date) < 10):
-            gig_date = "0" + gig_date
-        gig_month = each.find(class_='summary-thumbnail-event-date-month').string
-        when_string = gig_date + ' ' + gig_month
-        year_string = "2022"
-        if (today.month > datetime.strptime(gig_month, '%b').month):
-            year_string = str(today.year + 1)
-        gig["when"] = str(datetime.strptime(year_string + " " + when_string, '%Y %d %b'))
+        gig_date = each.select('gig-doors-open h2')[0].string.split()
+        gig_date.pop()
+        if (abs(gig_date[1]) < 10):
+            padded = '0' + gig_date[1]
+            gig_date[1] = padded
+        if (len(gig_date[3]) == 6):
+            pad_it = '0' + gig_date[3]
+            gig_date[3] = pad_it
+        gig["when"] = str(datetime.strptime(' '.join(gig_date), '%a %d %b, %I:%M%p'))
         gigs_array.append(gig)
 
     gig_scraper([])
